cef8496ec348cb31e419e1ba8e96d3fec73d0ab9
Unknown <marin.boone@edu.ece.fr>
2018-04-15 16:29:30 +0200

    Projet

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -3,19 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projet.java.sem.pkg2;
+package javaapplication4;
 
 /**
  *
  * @author peti_
  */
-public class ProjetJavaSem2 {
+public class JavaApplication4 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a =0;
+        System.out.println(a);
+             
     }
     
 }
